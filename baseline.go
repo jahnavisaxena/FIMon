fe5c124457f481abf1959919ac55bba695af7f81
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-11-08 01:19:07 +0530

    Update baseline.go

--- baseline.go
+++ baseline.go
@@ -1,1 +1,27 @@
 
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func LoadBaseline(file string) (map[string]string, error) {
+	baseline := make(map[string]string)
+	f, err := os.Open(file)
+	if err != nil {
+		return baseline, err
+	}
+	defer f.Close()
+	err = json.NewDecoder(f).Decode(&baseline)
+	return baseline, err
+}
+
+func SaveBaseline(file string, baseline map[string]string) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewEncoder(f).Encode(baseline)
+}
